ca426bad32d3add6cefa56fb67a86ccaa1f2ace3
Xuuuuu <18382705187@163.com>
2018-10-26 23:50:05 +0800

    Third update

---
+++ src/Target/ExplanatorNote.java
@@ -0,0 +1,29 @@
+package Target;
+import java.lang.String;
+/**
+ * 这是一个类注释
+ * @author Xuuuuu
+ */
+
+public class ExplanatorNote {
+	
+	/**
+	 * 这是一个值注释
+	 */
+	public static int a = 1;
+	
+	/**
+	 * 这是一个方法注释
+	 * @return 0
+	 * @param 传入一个int类型的数据
+	 */
+	public int getExplanatorNote(int x) {
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
